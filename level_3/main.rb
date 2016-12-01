3b6771d6fc9b7280c12da20b7ed02c61cd198eb9
marcot_f <felix.marcotte-ruffin@epitech.eu>
2016-12-01 16:45:33 +0100

    Challenges done.

--- level_3/main.rb
+++ level_3/main.rb
@@ -1,5 +1,15 @@
-require_relative "person"
+require_relative 'person'
+require 'csv'
 
-source_path = File.join(__dir__, "source.csv")
+source_path = File.join(__dir__, 'source.csv')
 
 # it's up to you now!
+## Done with Ruby v2.3.1p112
+dudes = Array.new
+CSV.foreach(source_path) do |row|
+  dudes.push(Person.new(row[0], row[1], row[2], row[3]))
+end
+
+dudes.each do |dude|
+  puts dude.print
+end
